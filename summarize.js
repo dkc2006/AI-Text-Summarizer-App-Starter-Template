b4c88c7228fab8015e62d79b285aeac4fa0e8b84
Deepanshu Khandelwal <khandelwaldeepanshu6@gmail.com>
2024-04-21 15:33:08 +0530

    move

--- summarize.js
+++ summarize.js
@@ -1,5 +1,10 @@
+const axios = require('axios');
+
 // This is the function where the call to the API is made. Returns the summarized text as a string.
+
 async function summarizeText(text) {
+
+  // INSERT CODE SNIPPET FROM POSTMAN BELOW
 
   let data = JSON.stringify({
     "inputs": text,
@@ -8,25 +13,31 @@ async function summarizeText(text) {
       "min_length": 30
     }
   });
-
+  
+  
   let config = {
     method: 'post',
     maxBodyLength: Infinity,
     url: 'https://api-inference.huggingface.co/models/facebook/bart-large-cnn',
     headers: { 
       'Content-Type': 'application/json', 
-      'Authorization': `Bearer ${process.env.ACCESS_TOKEN}` 
+      'Authorization': 'Bearer ' + process.env['ACCESS_TOKEN']
     },
     data : data
   };
 
-  try {
-    const response = await axios.request(config);
-    return response.data[0].summary_text;
-  } catch (error) {
-    console.log(error);
-  }
+ 
+    try {
+      const response = await axios.request(config);
+     return response.data[0].summary_text;
+    }
+    catch (error) {
+      console.log(error);
+    }
+
+
 }
 
 // Allows for summarizeText() to be called outside of this file
+
 module.exports = summarizeText;
